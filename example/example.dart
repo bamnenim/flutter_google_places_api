0348479ea4671cff24ccc4fc8829e3558915379f
bamnenim <bamnenim@gmail.com>
2020-04-06 19:43:06 +0900

    place autocomplete example

--- example/example.dart
+++ example/example.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_google_places_api/flutter_google_places_api.dart';
 
-void main(){
+void main() async {
+  var key = 'YOUR_API_KEY';
+  var input = 'YOUR_INPUT_HERE';
+  var response = await PlaceAutocompleteRequest(key: key, input: input).call();
+  print(response.predictions[0].description);
 }
